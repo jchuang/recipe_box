01c9842d9dec61b129100da420a4b40e09572f0b
Janet Chuang <janet.chuang@gmail.com>
2013-12-29 21:08:08 -0500

    first commit, Rails 4.0.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecipeBox::Application.config.secret_key_base = 'e0d3cb25f6074255c68bb028c2cc40c8b5722806eb90c2c278fd7f54f57c2652d6553b37f680f5f872811602bcdba172ec7804b1d4b91a75ad60f5d5e23096b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RecipeBox::Application.config.session_store :cookie_store, key: '_recipe_box_session'
