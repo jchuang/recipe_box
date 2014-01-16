d375d1dd8743bc4d653e049e48a311778bc153ca
Janet Chuang <janet.chuang@gmail.com>
2014-01-16 13:07:58 -0500

    add uniqueness validation to recipe tags

--- app/models/recipe_tag.rb
+++ app/models/recipe_tag.rb
@@ -2,6 +2,7 @@ class RecipeTag < ActiveRecord::Base
 
   validates :recipe, presence: true
   validates :tag, presence: true
+  validates :recipe_id, uniqueness: { scope: :tag_id }
 
   belongs_to :recipe, inverse_of: :recipe_tags
   belongs_to :tag, inverse_of: :recipe_tags
--- spec/models/recipe_tag_spec.rb
+++ spec/models/recipe_tag_spec.rb
@@ -5,6 +5,11 @@ describe RecipeTag do
   it { should validate_presence_of :recipe }
   it { should validate_presence_of :tag }
 
+  it 'should validate uniqueness of tags for a given recipe' do
+    FactoryGirl.create(:recipe_tag)
+    should validate_uniqueness_of(:recipe_id).scoped_to(:tag_id)
+  end
+
   it { should belong_to :recipe }
   it { should belong_to :tag }
 
