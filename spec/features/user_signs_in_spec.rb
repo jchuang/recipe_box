727efa5a2c38974b6a05f4a7439cdb1dc35d7074
Janet Chuang <janet.chuang@gmail.com>
2014-01-18 20:28:38 -0500

    handle unsuccessful signin attempts

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -25,4 +25,44 @@ feature 'registered user signs in', %q{
     expect(page).to have_content 'Sign Out'
   end
 
+  scenario 'email and password are not associated with an account' do
+    visit root_path
+    click_on 'Sign In'
+
+    fill_in 'Email Address', with: 'email@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Sign In'
+
+    expect(page).to_not have_content 'Welcome back!'
+    expect(page).to_not have_content 'Sign Out'
+  end
+
+  scenario 'existing email but incorrect password' do
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_on 'Sign In'
+
+    fill_in 'Email Address', with: user.email
+    fill_in 'Password', with: "#{user.password}abc"
+    click_button 'Sign In'
+
+    expect(page).to have_content 'Incorrect password, please try again.'
+    expect(page).to_not have_content 'Welcome back!'
+    expect(page).to_not have_content 'Sign Out'
+  end
+
+  scenario 'authenticated user cannot sign in again' do
+    user = FactoryGirl.create(:user)
+    visit new_user_session_path
+
+    fill_in 'Email Address', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+
+    expect(page).to have_content 'Sign Out'
+    expect(page).to_not have_content 'Sign In'
+
+    visit new_user_session_path
+    expect(page).to have_content 'You are already signed in.'
+  end
 end
